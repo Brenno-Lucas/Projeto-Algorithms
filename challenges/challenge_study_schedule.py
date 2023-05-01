bdd71d832e50c9b215c0ef86ed8774ca03e18c3d
Brenno-Lucas <brennolucas8@gmail.com>
2023-05-01 10:10:26 -0300

    Requisitos 01-02

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    try:
+        count = sum(
+            1 for start, end in permanence_period
+            if start <= target_time <= end
+        )
+        return count
+    except TypeError:
+        return None
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,9 @@
-# from challenges.challenge_encrypt_message import encrypt_message
-
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(23, "ABCBA")
+    assert encrypt_message("ABCBA", 23) == "ABCBA"
+    assert encrypt_message("ABCBA", 2) == "ABC_BA"
+    assert encrypt_message("ABCA", 3) == "CBA_A"
